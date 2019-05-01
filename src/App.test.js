728f5dd75a253da4d54f670d3a0d4718385ac491
Chandra <chandra.asar@gmail.com>
2019-05-01 10:48:08 -0400

    committing after configuring test dependencies and including first test. I might reorganize the file structure before continuing on this kata

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { expect } from 'chai';
+import { spy } from 'sinon';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('<App />', () => {
+  it('allows us to set props', () => {
+    const wrapper = Enzyme.mount(<App bar="baz" />);
+    expect(wrapper.props().bar).to.equal('baz');
+    wrapper.setProps({ bar: 'foo' });
+    expect(wrapper.props().bar).to.equal('foo');
+  });
 });
