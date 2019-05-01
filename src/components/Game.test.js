993ccb9e26d3c828f35d6d2b4b95a9360a4047ae
Chandra <chandra.asar@gmail.com>
2019-05-01 12:16:43 -0400

    A couple more files in the commit than intended because of some file structuring

--- src/components/Game.test.js
+++ src/components/Game.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../src/App';
+import Game from '../src/Game';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { expect } from 'chai';
@@ -8,9 +8,9 @@ import { spy } from 'sinon';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<App />', () => {
+describe('<Game />', () => {
   it('allows us to set props', () => {
-    const wrapper = Enzyme.mount(<App bar="baz" />);
+    const wrapper = Enzyme.mount(<Game bar="baz" />);
     expect(wrapper.props().bar).to.equal('baz');
     wrapper.setProps({ bar: 'foo' });
     expect(wrapper.props().bar).to.equal('foo');
