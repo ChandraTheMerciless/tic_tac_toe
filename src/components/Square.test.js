c59bd5167490e754cb482f6332087cf445a41d31
Chandra <chandra.asar@gmail.com>
2019-05-01 12:29:28 -0400

    Added test for Square

---
+++ src/components/Square.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Square from './Square';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { expect } from 'chai';
+import { spy } from 'sinon';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('<Square />', () => {
+  it('allows us to set props', () => {
+    const wrapper = Enzyme.mount(<Square bar="baz" />);
+    expect(wrapper.props().bar).to.equal('baz');
+    wrapper.setProps({ bar: 'foo' });
+    expect(wrapper.props().bar).to.equal('foo');
+  });
+});
