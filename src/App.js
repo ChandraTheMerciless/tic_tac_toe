55c25bbaacb1046e5437e2845a291134b98ca4ba
Chandra <chandra.asar@gmail.com>
2019-05-01 14:04:32 -0400

    added functionality to include game in the app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Game from './components/Game'
 import './App.css';
 
 export default class App extends React.Component {
@@ -8,7 +9,7 @@ export default class App extends React.Component {
   render(){
     return (
       <div className="App">
-
+        <Game />
       </div>
     );
   }
