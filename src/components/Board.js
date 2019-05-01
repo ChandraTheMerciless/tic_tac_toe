95c6029c9cb23646437c7d818ceebb92a4ace025
Chandra <chandra.asar@gmail.com>
2019-05-01 13:24:28 -0400

    Added Board file - no functionality yet, just base file to make base test pass

---
+++ src/components/Board.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import './Board.css';
+
+export default class Board extends React.Component {
+
+  render(){
+    return (
+      <div className="Board">
+        // Board will go here
+      </div>
+    );
+  }
+}
