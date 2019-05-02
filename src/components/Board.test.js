b5e8f410b30e1d7de2762c807695c349fed2745b
Chandra <chandra.asar@gmail.com>
2019-05-02 15:29:57 -0400

    refactored Board.test.js to mount the Board component for each test so that a state change in one test doesn't propogate to other tests

--- src/components/Board.test.js
+++ src/components/Board.test.js
@@ -10,34 +10,39 @@ import { spy } from 'sinon';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Board />', () => {
-  const wrapper = Enzyme.mount(<Board bar="baz" />);
   it('allows us to set props', () => {
+    const wrapper = Enzyme.mount(<Board bar="baz" />);
     expect(wrapper.props().bar).to.equal('baz');
     wrapper.setProps({ bar: 'foo' });
     expect(wrapper.props().bar).to.equal('foo');
   });
 
   it('allows us to render the Square component', () => {
+    const wrapper = Enzyme.mount(<Board />);
     expect(wrapper.containsMatchingElement(<Square />)).to.equal(true);
   });
 
   it('contains 9 squares, each set to null on load', () => {
+    const wrapper = Enzyme.mount(<Board />);
     expect(wrapper.state('squares').length).to.equal(9);
     expect(wrapper.state('squares')[0]).to.equal(null);
   });
 
   it('allows us to see the Square state in the Board component', () =>
   {
+    const wrapper = Enzyme.mount(<Board />);
     expect(wrapper.state('squares')[0]).to.equal(null);
     wrapper.find('button').first().simulate('click');
     expect(wrapper.state('squares')[0]).to.equal('X');
   });
 
   it('allows us to set X as the next player on load', () =>{
+    const wrapper = Enzyme.mount(<Board />);
     expect(wrapper.state('xGoesNext')).to.equal(true);
   });
 
   it('allows us to set O as the next player after on square is clicked', () =>{
+    const wrapper = Enzyme.mount(<Board />);
     wrapper.find('button').first().simulate('click');
     expect(wrapper.state('xGoesNext')).to.equal(false);
   });
