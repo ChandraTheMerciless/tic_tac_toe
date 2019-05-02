a18e3a7edbf96a220a355da15b0b14342d626ba8
Chandra <chandra.asar@gmail.com>
2019-05-02 10:33:55 -0400

    updated state handling in renderSquare function. Previously written unit test already covers this code

--- src/components/Board.js
+++ src/components/Board.js
@@ -11,7 +11,7 @@ export default class Board extends React.Component {
   }
 
   renderSquare(i){
-    return <Square value={i} />;
+    return <Square value={this.state.squares[i]} />;
   }
 
   render(){
